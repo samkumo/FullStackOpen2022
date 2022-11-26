fed2ef73db962e5a2bec5e5c3da1badb940e22ad
Samuli Kumo <samuli.kumo@gmail.com>
2022-11-26 20:36:21 +0100

    exercise 1.6

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const App = () => {
+  // save clicks of each button to it's own state
+  const [good, setGood] = useState(0)
+  const [neuttral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App;
