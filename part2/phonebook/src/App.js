5c88ae446c08e7a33252d688df083c9dd5596e9d
Samuli Kumo <samuli.kumo@gmail.com>
2022-11-28 18:53:56 +0100

    Exercises up to 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,54 @@
 import { useState } from 'react'
 
+//Separate component 1: Submission form
+const SubmitForm = (prop) => {
+  return (
+    <div>
+      <form onSubmit={prop.addName}>
+        <div>
+          name:
+          <input value={prop.newName} onChange={prop.handleNameChange}></input>
+        </div>
+        <div>
+          number:
+          <input
+            value={prop.newNumber}
+            onChange={prop.handleNumberChange}
+          ></input>
+        </div>
+        <div>
+          <button type="submit" onSubmit={prop.addName}>
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+//Separated component 2: List of people
+const Persons = ({ persons }) => {
+  return persons.map((person) => (
+    <Person key={person.key} person={person}></Person>
+  ))
+}
+//Separated component 2: Component for rendering single person
+const Person = ({ person }) => {
+  return (
+    <div>
+      {person.name} || {person.number}
+    </div>
+  )
+}
+
 const App = () => {
-  const [persons, setPersons] = useState([{ key: 0, name: 'Arto Hellas' }])
+  const [persons, setPersons] = useState([
+    { key: 0, name: 'Arto Hellas', number: '040-123456' },
+    { key: 1, name: 'Ada Lovelace', number: '39-44-5323523' },
+    { key: 2, name: 'Dan Abramov', number: '12-43-234345' },
+    { key: 3, name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -17,43 +63,46 @@ const App = () => {
     const personObject = {
       key: persons.length,
       name: newName,
+      number: newNumber,
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
-  const isNameDuplicate = (name) => {
-    const duplicates = persons.filter((person) => person.name === name)
+
+  const isNameDuplicate = (newName) => {
+    const duplicates = persons.filter((person) => person.name === newName)
     return duplicates.length !== 0 //Returns TRUE if duplicates found
   }
   const isNameEmpty = (name) => {
     return name === '' // Returns TRUE if name is initial
   }
+
   const handleNameChange = (event) => {
     event.preventDefault()
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    event.preventDefault()
+    setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name:
-          <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit" onSubmit={addName}>
-            add
-          </button>
-        </div>
-      </form>
+      {
+        <SubmitForm
+          handleNameChange={handleNameChange}
+          handleNumberChange={handleNumberChange}
+          addName={addName}
+          newName={newName}
+          newNumber={newNumber}
+        ></SubmitForm>
+      }
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => {
-          return <div key={person.key}>{person.name}</div>
-        })}
+        <Persons persons={persons}></Persons>
       </div>
-      <p>debug newName: {newName}</p>
     </div>
   )
 }
