026871e6d2b59b047d64e1ab83da3df708a84570
Samuli Kumo <samuli.kumo@gmail.com>
2022-11-29 20:25:07 +0100

    part2 - country_info initial commit

---
+++ part2/countries/src/App.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Countries</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ part2/countries/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+//Terminal 1 - JSON-server: npm run server
+//Terminal 2 - Application: npm start
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App></App>)
