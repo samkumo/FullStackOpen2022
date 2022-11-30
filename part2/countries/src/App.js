e09d8df1272ce09f60bfc8d135d7a60ca18b0a4e
Samuli Kumo <samuli.kumo@gmail.com>
2022-11-30 22:49:25 +0100

    Part 2: Countries, step2 and step3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,18 +4,50 @@ import axios from 'axios'
 const url_all = 'https://restcountries.com/v3.1/all'
 const url_name = 'https://restcountries.com/v3.1/name/'
 
-const Search = (props) => {
+//Weather functions
+function getWeatherUrl(lat, lon, key) {
+  const url =
+    'https://api.openweathermap.org/data/2.5/weather?' +
+    'units=metric' +
+    '&lat=' +
+    lat +
+    '&lon=' +
+    lon +
+    '&appid=' +
+    key
+  console.log(url)
+  return url
+}
+const Weather = (props) => {
+  const [weather, setWeather] = useState([])
+  const [temp, setTemp] = useState()
+  const [icon, setIcon] = useState()
+  const [wind, setWind] = useState()
+  const apikey = process.env.REACT_APP_API_KEY
+  const weatherIcon = 'http://openweathermap.org/img/wn/'
+  const lat = props.country.capitalInfo.latlng[0]
+  const lon = props.country.capitalInfo.latlng[1]
+
+  const hook = () => {
+    axios.get(getWeatherUrl(lat, lon, apikey)).then((response) => {
+      //console.log('axios: ', response.data.main.temp)
+      setWeather(response.data)
+      setTemp(response.data.main.temp)
+      setIcon(response.data.weather[0].icon)
+      setWind(response.data.wind.speed)
+    })
+  }
+  useEffect(hook, [])
   return (
-    <form onSubmit={props.searchCountries}>
-      Find Countries
-      <input
-        value={props.searchStr}
-        onChange={props.handleSearchStrChange}
-      ></input>
-    </form>
+    <div>
+      <p>Temperature: {Math.round(temp)} 'C</p>
+      <img src={weatherIcon + icon + '.png'}></img>
+      <p>Wind: {wind} m/s</p>
+    </div>
   )
 }
 
+// Country functions
 const Countries = ({ countries }) => {
   if (countries.length === 1) {
     return <CountryDetail country={countries[0]}></CountryDetail>
@@ -41,25 +73,7 @@ const CountryList = ({ countries }) => {
     return <Country key={country.cca3} country={country}></Country>
   })
 }
-const Languages = ({ languages }) => {
-  //Transforming language object into array just to make it easier to handle
-  const array = []
-  Object.keys(languages).forEach(function (key, index) {
-    array.push([key, languages[key]])
-  })
-
-  return (
-    <ul>
-      {array.map((language) => {
-        console.log(language)
-        return <li key={language[0]}>{language[1]}</li>
-      })}
-    </ul>
-  )
-}
-
 const CountryDetail = ({ country }) => {
-  console.log(country.languages)
   return (
     <div>
       <h2>{country.name.common}</h2>
@@ -70,13 +84,44 @@ const CountryDetail = ({ country }) => {
       <h3>Languages:</h3>
       <Languages languages={country.languages}></Languages>
       <img src={country.flags.png}></img>
+      <h3>Weather in {country.capital}</h3>
+      <Weather country={country}></Weather>
     </div>
   )
 }
+const Languages = ({ languages }) => {
+  //Transforming language object into array just to make it easier to handle
+  const array = []
+  Object.keys(languages).forEach(function (key, index) {
+    array.push([key, languages[key]])
+  })
+
+  return (
+    <ul>
+      {array.map((language) => {
+        return <li key={language[0]}>{language[1]}</li>
+      })}
+    </ul>
+  )
+}
+
+//Other functions
 const ErrorDisplay = ({ text }) => {
   return <p>{text}</p>
 }
+const Search = (props) => {
+  return (
+    <form onSubmit={props.searchCountries}>
+      Find Countries
+      <input
+        value={props.searchStr}
+        onChange={props.handleSearchStrChange}
+      ></input>
+    </form>
+  )
+}
 
+//Main App
 const App = () => {
   const [countries, setCountries] = useState([]) //All countries
   const [countriesFilter, setCountriesFilter] = useState([]) //Selected subset of countries
@@ -87,6 +132,8 @@ const App = () => {
       setCountries(response.data)
     })
   }
+  useEffect(hook, [])
+
   const handleSearchStrChange = (event) => {
     event.preventDefault()
     setSearchStr(event.target.value)
@@ -101,7 +148,6 @@ const App = () => {
       })
     )
   }
-  useEffect(hook, [])
 
   return (
     <div className="App">
