cfc39480cfd40facd5a668cd07a528dbea680839
Samuli Kumo <samuli.kumo@gmail.com>
2022-12-01 22:49:56 +0100

    2.16 Phonebook step-8

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -13,4 +13,5 @@ const update = (id, newObject) => {
   const request = axios.put('${baseUrl}/${id}', newObject)
   return request.then((response) => response.data)
 }
+
 export default { getAll, create, update }
