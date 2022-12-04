db54daf025703ad6440c1906e38dfbe0c90d5a73
Samuli Kumo <samuli.kumo@gmail.com>
2022-12-04 21:14:11 +0100

    3.4 3.5 Phonebook

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,6 +2,7 @@ const http = require("http")
 const PORT = 3001
 const express = require("express")
 const { allowedNodeEnvironmentFlags } = require("process")
+const { generateKey } = require("crypto")
 const app = express()
 
 let persons = [
@@ -45,7 +46,34 @@ app.get("/info", (request, response) => {
     const msg = "Phonebook has info for " + persons.length + " people <br>" + new Date()
     response.send(msg)
 })
+app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+app.post("/api/persons", (request, response) => {
+    //Validate that message is not empty
+    const body = request.body
+    if (!body.name || !body.number) {
+        return response.status(400).json({ error: "content missing" })
+    }
 
+    //Create person entry
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+    response.json(person)
+})
+
+//
+// Helper functions
+//
+const generateId = () => {
+    return Math.floor(Math.random() * 100000)
+}
 
 //
 // Run
