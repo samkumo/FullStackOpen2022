f88bb3813a3382a29be05bf756f14ae517eaf337
Samuli Kumo <samuli.kumo@gmail.com>
2022-12-23 00:51:34 +0100

    Part4 Notes initial commit

---
+++ part4/notes/backend/models/note.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const baseUrl = process.env.MONGODB_URI
+const pw = process.env.MONGODB_PW
+const app = 'noteApp'
+const url = baseUrl.replace('<password>', pw).replace('<app>', app)
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log('error connecting to MongoDB:', err.message);
+    })
+
+const noteSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        minLength: 5,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
---
+++ part4/notes/frontend/src/App.js
@@ -0,0 +1,80 @@
+import Note from './components/Note'
+import { useState, useEffect } from 'react'
+import noteService from './services/notes'
+import "./index.css"
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes)
+    })
+  }, [])
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+    }
+
+    noteService.create(noteObject).then((returnednote) => {
+      setNotes(notes.concat(returnednote))
+      setNewNote('')
+    })
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true)
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const toggleImportanceOf = (id) => {
+    const url = `/notes/${id}`
+    const note = notes.find((n) => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then((returnednote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnednote)))
+      })
+      .catch((error) => {
+        alert("This note '" + note.content + "' does not exist anymore")
+        setNotes(notes.filter((n) => n.id !== id))
+      })
+  }
+
+  return (
+    <div className="App">
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}></input>
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
