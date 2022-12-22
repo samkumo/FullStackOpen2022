e327157c064c980312ccd7af80ada4fa18c47a77
Samuli Kumo <samuli.kumo@gmail.com>
2022-12-22 23:18:03 +0100

    Part3 notes deployment

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -11,13 +11,10 @@ const create = (newObject) => {
   return request.then((response) => response.data)
 }
 const update = (id, newObject) => {
-  //const request = axios.put('${baseUrl}/${id}', newObject)
-  //const request = axios.put(baseUrl + '/' + id, newObject)
   const request = axios.put(`${baseUrl}/${id}`, newObject)
   return request.then((response) => response.data)
 }
 const deletePerson = (id) => {
-  //const request = axios.delete(baseUrl + '/' + id)
   const request = axios.delete(`${baseUrl}/${id}`)
   return request.then((response) => response.data)
 }
--- part3/notes/index.js
+++ part3/notes/index.js
@@ -43,6 +43,8 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
     next(error)
 }
@@ -61,22 +63,23 @@ app.get("/api/notes", (request, response) => {
     })
 })
 app.get("/api/notes/:id", (request, response, next) => {
-    const id = Number(request.params.id)
     Note.findById(request.params.id)
         .then(note => {
-            note ? response.json(note) : response.status(404).end()
+            note ? response.json(note.toJSON()) : response.status(404).end()
         })
         .catch(error => next(error))
 })
-app.delete("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id)
+app.delete("/api/notes/:id", (request, response, next) => {
     Note.findByIdAndDelete(response.params.id)
         .then(result => response.status(204).end())
         .catch(error => next(error))
 })
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
     //Validate that message is not empty
     const body = request.body
+    if (body.content === undefined) {
+        return response.status(400).json({ error: 'content missing' })
+    }
     if (!body.content) {
         return response.status(400).json({
             error: "content missing"
@@ -88,9 +91,11 @@ app.post("/api/notes", (request, response) => {
         important: body.important || false,
         date: new Date(),
     })
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote.toJSON())
+        })
+        .catch(error => next(error))
 })
 app.put('/api/notes/:id', (request, response, next) => {
     const body = request.body
@@ -98,9 +103,12 @@ app.put('/api/notes/:id', (request, response, next) => {
         content: body.content,
         important: body.important
     }
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(
+        request.params.id,
+        { content, important },
+        { new: true, runValidators: true, context: 'query' })
         .then(updatedNote => {
-            response.json(updatedNote)
+            response.json(updatedNote.toJSON())
         })
         .catch(error => next(error))
 })
