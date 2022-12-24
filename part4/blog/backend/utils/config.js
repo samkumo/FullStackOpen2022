f4413eff9f283eb7bc94aa9bd7a100c627134d6f
Samuli Kumo <samuli.kumo@gmail.com>
2022-12-24 15:16:29 +0100

    4.3 Blogs tests

--- part4/blog/backend/utils/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-const PORT = process.env.port || 3003
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/blog'
-const MONGODB_PW = process.env.MONGODB_PW
-const APP = 'blogApp'
-
-module.exports = { PORT, APP, MONGODB_URI, MONGODB_PW }
