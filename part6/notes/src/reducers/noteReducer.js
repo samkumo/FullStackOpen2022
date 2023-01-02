5d69e68918b2dc20361fd13e647db8f8a51fdbfc
Samuli Kumo <samuli.kumo@gmail.com>
2023-01-02 22:42:42 +0100

    6.2 unicafe-redux

--- part6/notes/src/reducers/noteReducer.js
+++ part6/notes/src/reducers/noteReducer.js
@@ -1,7 +1,7 @@
 const noteReducer = (state = [], action) => {
     switch (action.type) {
         case 'NEW_NOTE':
-            return state.concat(action.data)
+            return [...state, action.data]
         case 'TOGGLE_IMPORTANCE':
             const id = action.data.id
             const noteToChange = state.find(n => n.id === id)
