b979b9f1e2ef705390318ea4bccc21839d450f22
Samuli Kumo <samuli.kumo@gmail.com>
2023-01-03 19:52:44 +0100

    Part6 notes components

--- part6/notes/src/App.js
+++ part6/notes/src/App.js
@@ -1,88 +1,11 @@
-import { createStore } from 'redux'
-
-const noteReducer = (state = [], action) => {
-  switch (action.type) {
-    case 'NEW_NOTE':
-      return state.concat(action.data)
-    case 'TOGGLE_IMPORTANCE':
-      return state
-    default:
-      return state
-  }
-}
-const store = createStore(noteReducer)
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-store.dispatch({
-  type: 'TOGGLE_IMPORTANCE',
-  data: {
-    id: 2
-  }
-})
-
-const generateId = () => {
-  return Number((Math.random() * 1000000).toFixed(0))
-}
-const createNote = (content) => {
-  return {
-    type: 'NEW_NOTE',
-    data: {
-      content,
-      important: false,
-      id: generateId()
-    }
-  }
-}
-const toggleImportanceOf = (id) => {
-  store.dispatch({
-    type: 'TOGGLE_IMPORTANCE',
-    data: { id }
-  })
-}
+import NewNote from './components/NewNote'
+import Notes from './components/Notes'
 
 function App() {
-  const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    store.dispatch(createNote(content))
-  }
-  const toggleImportance = (id) => {
-    store.dispatch(toggleImportanceOf(id))
-  }
-
-
   return (
     <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {store.getState().map(note =>
-          <li
-            key={note.id}
-            onClick={() => toggleImportance(note.id)}
-          >
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </ul>
+      <NewNote></NewNote>
+      <Notes></Notes>
     </div>
   )
 }
